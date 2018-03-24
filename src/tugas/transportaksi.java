be3b3b5640b7f065e75250c4083db26b2ad24ed5
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2018-03-24 10:51:00 +0800

    Final.

--- src/tugas/transportaksi.java
+++ src/tugas/transportaksi.java
@@ -3,6 +3,8 @@ package tugas;
 public class transportaksi {
     public static void main(String[] args) {
         transportasiudara Pesawat = new transportasiudara();
+        transportasiudara Helikopter = new transportasiudara();
+        transportasiudara Roket = new transportasiudara();
         
         Pesawat.Jenis = "Airbus 747";
         Pesawat.Kelas = "First Class";
@@ -10,6 +12,20 @@ public class transportaksi {
         Pesawat.JmlAwak = 50;
         Pesawat.Warna = "Putih Pink";
         
+        Helikopter.Jenis = "Helikoper Tempur";
+        Helikopter.Kelas = "Tempur";
+        Helikopter.JmlKursi = 8;
+        Helikopter.JmlAwak = 8;
+        Helikopter.Warna = "Hijau";
+        
+        Roket.Jenis = "Roket Luar Angkasa";
+        Roket.Kelas = "Bussiness";
+        Roket.JmlKursi = 20;
+        Roket.JmlAwak = 20;
+        Roket.Warna = "Putih";
+        
         Pesawat.infotransport();
+        Helikopter.infotransport();
+        Roket.infotransport();
     }
 }
