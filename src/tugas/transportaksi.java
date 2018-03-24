6a2ef5547e0bdc215e6939204750cac00b71d2e8
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2018-03-24 10:45:20 +0800

    mengisi nilai atribut

--- src/tugas/transportaksi.java
+++ src/tugas/transportaksi.java
@@ -3,5 +3,11 @@ package tugas;
 public class transportaksi {
     public static void main(String[] args) {
         transportasiudara Pesawat = new transportasiudara();
+        
+        Pesawat.Jenis = "Airbus 747";
+        Pesawat.Kelas = "First Class";
+        Pesawat.JmlKursi = 30;
+        Pesawat.JmlAwak = 50;
+        Pesawat.Warna = "Putih Pink";
     }
 }
